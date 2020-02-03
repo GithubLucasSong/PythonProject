f7f679c28535d4882c7437f2e974f9db6ac3ef4c
Lucas <1433880147@qq.com>
2020-02-04 00:00:10 +0800

    2020年2月4日00点00分

--- live01/live01作业/07.py
+++ live01/live01作业/07.py
@@ -17,7 +17,11 @@ class Soldier():
         if self.gun == None:
             print('还没有枪')
         else:
-            self.gun.shoot()
+            while True:
+                if self.gun.shoot(self.name) == False:
+                    self.gun.add_buttle(self.name)
+                else:
+                    time.sleep(0.3)
 
 
 
@@ -25,21 +29,21 @@ class Gun(Soldier):
     def __init__(self,model,bullet_count):
         self.model = model
         self.bullet_count = bullet_count
-        
-    def shoot(self):
+
+    def shoot(self,name):
         if self.bullet_count > 0:
             self.bullet_count -= 1
-            print(f'{self.name}使用{self.model}开火,剩余{self.bullet_count}子弹')
+            print(f'{name}使用{self.model}开火,剩余{self.bullet_count}子弹')
 
         else:
             print(f'{self.model}没有子弹了')
+            return False
 
-    def add_buttle(self):
+    def add_buttle(self,name):
         self.bullet_count += 30
-        print(f'{self.model}装填子弹,剩余{self.bullet_count}子弹')
+        time.sleep(3)
+        print(f'{name}给{self.model}装填子弹,剩余{self.bullet_count}子弹')
 
 gun = Gun('AK47',30)
 person = Soldier('肥皂',gun)
-while True:
-    person.fire()
-    time.sleep(0.5)
+person.fire()
