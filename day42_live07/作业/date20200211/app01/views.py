c84d720ae743dac2d0a36f6d40e285f816f14c06
Lucas <1433880147@qq.com>
2020-02-12 00:37:38 +0800

    2020年2月12日00点37分

--- day42_live07/作业/date20200211/app01/views.py
+++ day42_live07/作业/date20200211/app01/views.py
@@ -3,8 +3,6 @@ from app01 import models
 
 # Create your views here.
 def login(request):
-    print(request.GET)
-
     if request.method == 'POST':
         # 处理POST请求的逻辑
         user = request.POST.get('une')
@@ -21,17 +19,4 @@ def login(request):
 
 
 def index(request):
-    # orm的测试
-    ret = models.User.objects.all()  #  获取表中所有的数据  QuerySet  对象列表
-    for i in ret:
-        print(i, i.username, i.password,type(i.username))
-
-    # ret = models.User.objects.get(username='alex',password='1234') # 获取一条数据
-    # ret = models.User.objects.get(password='123') # 获取一条数据  获取不到或者获取到多条数据 就报错了
-
-    ret = models.User.objects.filter(password='1234') # 获取满足条件的对象
-
-
-    print(ret, type(ret))
-
     return render(request, 'index.html')
